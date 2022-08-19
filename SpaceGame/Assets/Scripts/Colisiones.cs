72b23a6ea57b2a8221287e4a34ef4282d70ddb10
Angais <angelbarnesbogado@gmail.com>
2022-08-19 02:05:12 +0200

    Agitar-Camara&Particulas&Trail

---
+++ SpaceGame/Assets/Scripts/Colisiones.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateCollidersAcrossScreen();
+    }
+
+    void GenerateCollidersAcrossScreen()
+    {
+        GameObject padre = new GameObject("Colisiones");
+
+        Vector2 lDCorner = Camera.main.ViewportToWorldPoint(new Vector3(0, 0f, Camera.main.nearClipPlane));
+        Vector2 rUCorner = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Camera.main.nearClipPlane));
+        Vector2[] colliderpoints;
+
+        EdgeCollider2D upperEdge = new GameObject("upperEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = upperEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, rUCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, rUCorner.y);
+        upperEdge.points = colliderpoints;
+
+        EdgeCollider2D lowerEdge = new GameObject("lowerEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = lowerEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+        lowerEdge.points = colliderpoints;
+
+        EdgeCollider2D leftEdge = new GameObject("leftEdge").AddComponent<EdgeCollider2D>();
+        colliderpoints = leftEdge.points;
+        colliderpoints[0] = new Vector2(lDCorner.x, lDCorner.y);
+        colliderpoints[1] = new Vector2(lDCorner.x, rUCorner.y);
+        leftEdge.points = colliderpoints;
+
+        EdgeCollider2D rightEdge = new GameObject("rightEdge").AddComponent<EdgeCollider2D>();
+
+        colliderpoints = rightEdge.points;
+        colliderpoints[0] = new Vector2(rUCorner.x, rUCorner.y);
+        colliderpoints[1] = new Vector2(rUCorner.x, lDCorner.y);
+        rightEdge.points = colliderpoints;
+
+        upperEdge.transform.SetParent(padre.transform);
+        lowerEdge.transform.SetParent(padre.transform);
+        leftEdge.transform.SetParent(padre.transform);
+        rightEdge.transform.SetParent(padre.transform);
+    }
+}
--- SpaceGame/Assets/Scripts/Movimiento.cs
+++ SpaceGame/Assets/Scripts/Movimiento.cs
@@ -8,11 +8,28 @@ public class Movimiento : MonoBehaviour
     [SerializeField] private float velocidad;
     [SerializeField] private float velocidadGiro;
 
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+    private Vector3 originalPos;
+    public float sensibilidad = 1;
+    public float tiempo = 0.1f;
+    public Transform PolvoPrefab;
+    public TrailRenderer trail;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalPos = camTransform.localPosition;
     }
 
     // Update is called once per frame
@@ -22,10 +39,40 @@ public class Movimiento : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
             Mover();
+
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+
+        if (rb.velocity.magnitude > 0)
+            trail.emitting = true;
+        else
+            trail.emitting = false;
     }
 
     private void Mover()
     {
         rb.AddForce(transform.up * velocidad * Time.deltaTime * 100);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (rb.velocity.magnitude > sensibilidad)
+        {
+            shakeDuration += tiempo;
+            if(collision.transform.CompareTag("Meteorito"))
+            {
+                Instantiate(PolvoPrefab, collision.GetContact(0).point, Quaternion.identity);
+            }
+
+        }
     }
 }
