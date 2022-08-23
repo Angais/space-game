4a6190eceac3537a567f6ede2860b141d8f40bc7
Angais <angelbarnesbogado@gmail.com>
2022-08-23 23:43:53 +0200

    Versión Final
    Proyecto terminado, siguientes actualizaciones serán bug fixes.

---
+++ SpaceGame/Spow_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,141 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Colisiones::Start()
+extern void Colisiones_Start_m86F7DD4C6B21A0B373B5279ED51D1E2F0FABBA7F (void);
+// 0x00000002 System.Void Colisiones::GenerateCollidersAcrossScreen()
+extern void Colisiones_GenerateCollidersAcrossScreen_m67FD685EA805216B8F3F81DBEE0F63175A2AA695 (void);
+// 0x00000003 System.Void Colisiones::.ctor()
+extern void Colisiones__ctor_m732C696749B9DA18C96650D06C2ACF5AB3210279 (void);
+// 0x00000004 System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x00000005 System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x00000006 System.Void GameManager::MostrarPanel()
+extern void GameManager_MostrarPanel_m3E075095B707838BC4C7FAF40C077764CDCA579A (void);
+// 0x00000007 System.Void GameManager::CargarNivel(System.Int32)
+extern void GameManager_CargarNivel_mB8BA23878CFFAA410B1B6FEAC883D2C350A5DFF3 (void);
+// 0x00000008 System.Void GameManager::MenosSkin()
+extern void GameManager_MenosSkin_m0095B8C942B72106081A5F1B3F1F2ACDF4D4DBF6 (void);
+// 0x00000009 System.Void GameManager::MasSkin()
+extern void GameManager_MasSkin_mA1ED6F1D68FD339BBA3C13D11F1C07C56E9B44F8 (void);
+// 0x0000000A System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x0000000B System.Void MoverFondo::Awake()
+extern void MoverFondo_Awake_mB884E8233D56767224164A16C95624C45B6161BD (void);
+// 0x0000000C System.Void MoverFondo::Update()
+extern void MoverFondo_Update_mBCB2A90931D224D1F39252BC638956D92EC31568 (void);
+// 0x0000000D System.Void MoverFondo::.ctor()
+extern void MoverFondo__ctor_mF267A576A7BBB8AD298EFEF6C9DD341FD9CF93C1 (void);
+// 0x0000000E System.Void Movimiento::Start()
+extern void Movimiento_Start_mEF29C0EF40FEC4E2860AC94A07A449D2600A9E96 (void);
+// 0x0000000F System.Void Movimiento::Update()
+extern void Movimiento_Update_mD64F4C6BC8088329F9BF937ADB85968906C53219 (void);
+// 0x00000010 System.Void Movimiento::Mover()
+extern void Movimiento_Mover_m70AE074E35AF68793075D9AE320EFC63F60D22EF (void);
+// 0x00000011 System.Void Movimiento::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void Movimiento_OnCollisionEnter2D_m71848DBCB370A32805D443B5FBADE3ACFCA7DE29 (void);
+// 0x00000012 System.Void Movimiento::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void Movimiento_OnTriggerEnter2D_m393F35EBAD1042D79DD00713AE270417D123CD91 (void);
+// 0x00000013 System.Void Movimiento::MenuPrincipal()
+extern void Movimiento_MenuPrincipal_m7DDA6937D3DD78430355EB07A4118B8D0F781463 (void);
+// 0x00000014 System.Void Movimiento::SiguienteNivel()
+extern void Movimiento_SiguienteNivel_m93CC077B3B3361CB1785292CDBC1C7A14C6B678C (void);
+// 0x00000015 System.Void Movimiento::ReiniciarNivel()
+extern void Movimiento_ReiniciarNivel_mD477EA19FE18C5666EBFC0321A8936BADC2F7B55 (void);
+// 0x00000016 System.Void Movimiento::.ctor()
+extern void Movimiento__ctor_mB8AD6B1B0E6D703DBD8830C937AB13EE9DDFE8A3 (void);
+// 0x00000017 System.Void Tamano::Start()
+extern void Tamano_Start_m4D7A43C9C664275785B3A8252320E2CC7957BDB1 (void);
+// 0x00000018 System.Void Tamano::Update()
+extern void Tamano_Update_mC5C8C55A88C76D78F04A85A5FB602DDC93CD64DB (void);
+// 0x00000019 System.Void Tamano::.ctor()
+extern void Tamano__ctor_m52DA3A8DAF2C7507F848510D7FC65E2953E57B3D (void);
+static Il2CppMethodPointer s_methodPointers[25] = 
+{
+	Colisiones_Start_m86F7DD4C6B21A0B373B5279ED51D1E2F0FABBA7F,
+	Colisiones_GenerateCollidersAcrossScreen_m67FD685EA805216B8F3F81DBEE0F63175A2AA695,
+	Colisiones__ctor_m732C696749B9DA18C96650D06C2ACF5AB3210279,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_MostrarPanel_m3E075095B707838BC4C7FAF40C077764CDCA579A,
+	GameManager_CargarNivel_mB8BA23878CFFAA410B1B6FEAC883D2C350A5DFF3,
+	GameManager_MenosSkin_m0095B8C942B72106081A5F1B3F1F2ACDF4D4DBF6,
+	GameManager_MasSkin_mA1ED6F1D68FD339BBA3C13D11F1C07C56E9B44F8,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	MoverFondo_Awake_mB884E8233D56767224164A16C95624C45B6161BD,
+	MoverFondo_Update_mBCB2A90931D224D1F39252BC638956D92EC31568,
+	MoverFondo__ctor_mF267A576A7BBB8AD298EFEF6C9DD341FD9CF93C1,
+	Movimiento_Start_mEF29C0EF40FEC4E2860AC94A07A449D2600A9E96,
+	Movimiento_Update_mD64F4C6BC8088329F9BF937ADB85968906C53219,
+	Movimiento_Mover_m70AE074E35AF68793075D9AE320EFC63F60D22EF,
+	Movimiento_OnCollisionEnter2D_m71848DBCB370A32805D443B5FBADE3ACFCA7DE29,
+	Movimiento_OnTriggerEnter2D_m393F35EBAD1042D79DD00713AE270417D123CD91,
+	Movimiento_MenuPrincipal_m7DDA6937D3DD78430355EB07A4118B8D0F781463,
+	Movimiento_SiguienteNivel_m93CC077B3B3361CB1785292CDBC1C7A14C6B678C,
+	Movimiento_ReiniciarNivel_mD477EA19FE18C5666EBFC0321A8936BADC2F7B55,
+	Movimiento__ctor_mB8AD6B1B0E6D703DBD8830C937AB13EE9DDFE8A3,
+	Tamano_Start_m4D7A43C9C664275785B3A8252320E2CC7957BDB1,
+	Tamano_Update_mC5C8C55A88C76D78F04A85A5FB602DDC93CD64DB,
+	Tamano__ctor_m52DA3A8DAF2C7507F848510D7FC65E2953E57B3D,
+};
+static const int32_t s_InvokerIndices[25] = 
+{
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	2809,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	2827,
+	2827,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+	3469,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	25,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
